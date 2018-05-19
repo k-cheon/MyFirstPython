3fabb2382d783536174eaf35fd15a2906bab8ee9
k-cheon <cheon.miru@gmail.com>
2018-05-19 20:52:17 +0900

    First commit

---
+++ Hello.py
@@ -0,0 +1,23 @@
+message = "Hello Python world!"
+print(message)
+message = "Hello Python Crash Course world"
+print(message)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+
+print("Hello Git!")
